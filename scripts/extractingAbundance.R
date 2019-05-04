95b6bc5c91db19aa20859bd64b46387b57bbb59e
aarkilanian <a.arkilanian@gmail.com>
2019-05-04 10:29:31 -0400

    added first iteration of abundance extraction code from inaturalist

---
+++ scripts/extractingAbundance.R
@@ -0,0 +1,36 @@
+##### Extracting bird abundance data from July 2018 across national parks #####
+
+require(rinat)
+locations <- read.csv("data/location.csv")
+
+final <- as.data.frame(cbind("Japser", get_inat_obs(taxon_id = 3, geo = TRUE, year = 2018, month = 7, bounds = c(locations$min.lat[7]-0.2,
+                                                                                   locations$min.lng[7]-0.2,
+                                                                                   locations$max.lat[7]+0.2,
+                                                                                   locations$max.lng[7]+0.2))[,1]))
+
+for (i in 8:17){
+  rbind(final,
+        cbind(
+          locations$parks[i],
+          get_inat_obs(taxon_id = 3, geo = TRUE, year = 2018, month = 7, bounds = c(locations$min.lat[i]-0.2,
+                                                                                    locations$min.lng[i]-0.2,
+                                                                                    locations$max.lat[i]+0.2,
+                                                                                    locations$max.lng[i]+0.2))[,1]
+        ))
+}
+
+## Running through individual data frames
+
+final <- as.data.frame(rbind(
+  cbind(as.character(locations$parks[7]), jasper$scientific_name),
+  cbind(as.character(locations$parks[8]), banff$scientific_name),
+  cbind(as.character(locations$parks[9]), waterton.lakes$scientific_name),
+  cbind(as.character(locations$parks[10]), north.cascades$scientific_name),
+  cbind(as.character(locations$parks[11]), mont.rainier$scientific_name),
+  cbind(as.character(locations$parks[12]), yellowstone$scientific_name),
+  cbind(as.character(locations$parks[13]), grand.bassin$scientific_name),
+  cbind(as.character(locations$parks[14]), zion$scientific_name),
+  cbind(as.character(locations$parks[15]), joshua.tree$scientific_name),
+  cbind(as.character(locations$parks[16]), saguaro$scientific_name),
+  cbind(as.character(locations$parks[17]), big.bend$scientific_name)
+))
